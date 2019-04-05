3b2acef102a48c3f5afe76661a9f2c826df442b1
ahemaesh <ahemaesh@andrew.cmu.edu>
2019-04-05 00:06:50 -0400

    ICP Upload

--- cuboid_detection/src/iterative_closest_point.cpp
+++ cuboid_detection/src/iterative_closest_point.cpp
@@ -18,16 +18,56 @@
 #include <sensor_msgs/point_cloud_conversion.h>
 #include <sensor_msgs/PointCloud.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
 
+
+using namespace std;
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
-static ros::Publisher icp_PCL_pub;
+static ros::Publisher icp_transform_pub;
+std::string template_cuboid_filename;
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr template_cuboid(new pcl::PointCloud<pcl::PointXYZ>);
-pcl::PointCloud<pcl::PointXYZ>::Ptr input_cuboid(new pcl::PointCloud<pcl::PointXYZ>);
 
-int performICP()
+void convert_icp_eigen_to_tf(Eigen::Matrix4f Tm)
 {
+  tf::Vector3 origin;
+  static tf::TransformBroadcaster br;
+
+  origin.setValue(static_cast<double>(Tm(0,3)),static_cast<double>(Tm(1,3)),static_cast<double>(Tm(2,3)));
+
+  cerr << origin << endl;
+  tf::Matrix3x3 tf3d;
+  tf3d.setValue(static_cast<double>(Tm(0,0)), static_cast<double>(Tm(0,1)), static_cast<double>(Tm(0,2)), 
+        static_cast<double>(Tm(1,0)), static_cast<double>(Tm(1,1)), static_cast<double>(Tm(1,2)), 
+        static_cast<double>(Tm(2,0)), static_cast<double>(Tm(2,1)), static_cast<double>(Tm(2,2)));
+
+  tf::Quaternion tfqt;
+  tf3d.getRotation(tfqt);
+
+  tf::Transform transform;
+  transform.setOrigin(origin);
+  transform.setRotation(tfqt);
+
+  br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "camera_depth_frame", "cuboid_frame"));
+}
+
+void rawPCL_cb(const sensor_msgs::PointCloud2::ConstPtr& msg)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr template_cuboid(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr input_cuboid(new pcl::PointCloud<pcl::PointXYZ>);
+
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(*msg,pcl_pc2);
+    pcl::fromPCLPointCloud2(pcl_pc2,*input_cuboid);
+   
+
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> (template_cuboid_filename, *template_cuboid) == -1) //* load the file
+    {
+        PCL_ERROR ("Couldn't read file");
+        return;
+    }
+    
     pcl::IterativeClosestPoint <pcl::PointXYZ, pcl::PointXYZ> icp;
     icp.setInputSource(input_cuboid);
     icp.setInputTarget(template_cuboid);
@@ -36,33 +76,18 @@ int performICP()
     std::cout << "has converged:" << icp.hasConverged() << " score: " <<
               icp.getFitnessScore() << std::endl;
     std::cout << icp.getFinalTransformation() << std::endl;
-
-    //icp_PCL_pub.publish(icp.getFinalTransformation());
-
-    return (0);
-}
-
-void rawPCL_cb(const sensor_msgs::PointCloud2::ConstPtr& msg)
-{
-    pcl::PCLPointCloud2 pcl_pc2;
-    pcl_conversions::toPCL(*msg,pcl_pc2);
-    pcl::fromPCLPointCloud2(pcl_pc2,*input_cuboid);
-    performICP();
+    convert_icp_eigen_to_tf(icp.getFinalTransformation());
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc,argv,"iterative_closest_point");
     ros::NodeHandle nh;
-    //icp_PCL_pub = nh.advertise<geometry_msgs::>("iterative_closest_point", 1000);
-    ros::Subscriber raw_PCL_sub = nh.subscribe<sensor_msgs::PointCloud2>("/ground_plane_segmentation/points", 1000, rawPCL_cb);
+    nh.getParam("template_cuboid_path", template_cuboid_filename);
 
-    if (pcl::io::loadPCDFile<pcl::PointXYZ> ("$/{workspaceFolder}/src/perception/cuboid_detection/templates/template_cuboid_L200_W100_H75.pcd", *template_cuboid) == -1) //* load the file
-    {
-        PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
-        return (-1);
-    }
+    ROS_INFO("%s",template_cuboid_filename.c_str());
+    //cerr << template_cuboid_filename << endl;
+    // ros::Subscriber raw_PCL_sub = nh.subscribe<sensor_msgs::PointCloud2>("/ground_plane_segmentation/points", 1000, rawPCL_cb);
     
-    ros::spinOnce();
-    return 0;
+    ros::spin();
 }
